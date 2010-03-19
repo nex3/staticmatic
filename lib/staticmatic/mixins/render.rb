34168f104b88ff5195b0afafb5c1200b43f91131
Nathan Weizenbaum <nex342@gmail.com>
2010-03-18 17:19:05 -0700

    Tell Sass::Engine what the Sass filename is.

--- lib/staticmatic/mixins/render.rb
+++ lib/staticmatic/mixins/render.rb
@@ -76,7 +76,10 @@ module StaticMatic::RenderMixin
   def generate_css(source, source_dir = '')
     full_file_path = File.join(@src_dir, 'stylesheets', source_dir, "#{source}.sass")
     begin
-      sass_options = { :load_paths => [ File.join(@src_dir, 'stylesheets') ] }.merge(self.configuration.sass_options)
+      sass_options = {
+        :load_paths => [ File.join(@src_dir, 'stylesheets') ],
+        :filename => full_file_path,
+      }.merge(self.configuration.sass_options)
       stylesheet = Sass::Engine.new(File.read(full_file_path), sass_options)
       stylesheet.to_css
     rescue Exception => e
